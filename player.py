ba76b64ee0b2f2e0e347021f8724c2d259338b55
Siddharth Pathak <siddharthpathak415@gmail.com>
2023-10-16 21:47:49 -0700

    added move choosing fns to class Player, HumanPlayer

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import math, random
 
 class Player():
     """
-    A class representing a player in a tic-tac-toe game.
+    A Super class representing a player in a tic-tac-toe game.
 
     Attributes:
     ----------
@@ -11,6 +11,9 @@ class Player():
     """
     def __init__(self, letter) -> None:
         self.letter = letter
+
+    def choose_move(self, game): 
+        pass
 
 class HumanPlayer(Player):
     """
@@ -23,6 +26,20 @@ class HumanPlayer(Player):
     """
     def __init__(self, letter) -> None:
         super().__init__(letter)
+
+    def choose_move(self, game):
+        validSquare = False
+        val = None
+        while not validSquare:
+            square = input(self.letter + '\'s turn. Input Move (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                validSquare = True
+            except ValueError:
+                print('Invalid Sqaure. Try Again.')
+        return val
 
 class ComputerPlayer(Player):
     """
