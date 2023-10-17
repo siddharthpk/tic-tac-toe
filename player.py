57cad96749cfe7b3d739ab46d78d707803cf7a57
Siddharth Pathak <siddharthpathak415@gmail.com>
2023-10-16 22:12:42 -0700

    added minimax algo code

--- player.py
+++ player.py
@@ -54,7 +54,7 @@ class ComputerPlayer(Player):
         super().__init__(letter)
     
     def choose_move(self, game):
-        square = random.choice(game.available_moves()))=
+        square = random.choice(game.available_moves())
         return square
 
 class AIComputerPlayer(Player):
@@ -74,3 +74,50 @@ class AIComputerPlayer(Player):
             square = self.minimax(game,self.letter)['position']
         return square
     
+    def minimax(self, state, player):
+        max_Player = self.letter
+        min_Player = '0' if player == 'X' else 'X'
+        utility_Fn = [0,1,-1]
+
+        if state.current_winner == min_Player:
+            return {
+                'position' : None,
+                'score':utility_Fn[1]*(state.num_empty_squares()+ 1) if min_Player == max_Player 
+                        else utility_Fn[-1]*(state.num_empty_squares() + 1)
+            }
+        elif not state.empty_squares():
+            return {
+                'position' : None,
+               
+                'score': utility_Fn[0]
+            }
+        
+        if player == max_Player:
+            best = {
+                'position': None,
+                'score': -math.inf
+            }
+        else:
+            best = {
+                'position': None,
+                'score': math.inf
+            }
+
+        for possible_move in state.available_moves():
+            # Making a simulated move
+            state.make_move(possible_move, player)
+            simulated_score = self.minimax(state, min_Player)
+
+            # Undoing move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            simulated_score['position'] = possible_move
+
+            if player == max_Player:
+                if simulated_score['score'] > best['score']:
+                    best = simulated_score
+            else:
+                if simulated_score['score'] < best['score']:
+                    best = simulated_score
+        return best
+
